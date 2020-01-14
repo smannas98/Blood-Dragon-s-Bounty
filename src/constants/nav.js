a796a9cbd3a642321c2fd2bfab6756d17b2f6ab7
Sean Mannas <seanmannas02@gmail.com>
2020-01-14 17:24:50 -0500

    first commit of project structure

---
+++ src/constants/nav.js
@@ -0,0 +1,25 @@
+import {
+  faHome,
+  faBell,
+  faChartBar,
+  faUser
+} from "@fortawesome/free-solid-svg-icons";
+
+export const nav = [
+  {
+    icon: faHome,
+    path: "/"
+  },
+  {
+    icon: faBell,
+    path: "/alerts"
+  },
+  {
+    icon: faChartBar,
+    path: "/stats"
+  },
+  {
+    icon: faUser,
+    path: "/profile"
+  }
+];
